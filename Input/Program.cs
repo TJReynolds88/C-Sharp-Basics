d3a7b8afff6a271c7220ec9d1682b5735ac735fa
TJ Reynolds <tj.reynolds1988@gmail.com>
2020-10-03 16:38:42 -0500

    Add files via upload

--- Input/Program.cs
+++ Input/Program.cs
@@ -1,1 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asking Users Name
+            Console.Title = "Input";
+            Console.Write("Please Enter Your Name.");
+            string name = Console.ReadLine();
+
+            // Printing Users Input
+            Console.WriteLine("Welcome " + name + "!");
+            Console.WriteLine("Hope you enjoy coding in C#");
+            Console.ReadKey();
+
+        }
+    }
+}
