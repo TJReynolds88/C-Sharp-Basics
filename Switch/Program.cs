624d731bc9f688ccf9d9c3df3e3170abc086f8ed
TJ Reynolds <tj.reynolds1988@gmail.com>
2020-10-03 16:37:11 -0500

    Add files via upload

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -1,1 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Switch";
+
+            int num = 6;
+
+            string day;
+
+            switch(num)
+            {
+                case 1: day = "Monday"; break;
+                case 2: day = "Tuesday"; break;
+                case 3: day = "Wenesday"; break;
+                case 4: day = "Thursday"; break;
+                case 5: day = "Friday"; break;
+                default : day = "Weekend Day"; break;
+
+            }
+
+            Console.WriteLine("Day " + num + " : " + day);
+            Console.ReadKey();
+
+        }
+    }
+}
