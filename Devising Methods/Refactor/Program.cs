4d138856aea6ebfdbe855c2da468da412fe07ed4
TJ Reynolds <tj.reynolds1988@gmail.com>
2020-10-05 21:10:49 -0500

    Add files via upload

--- Devising Methods/Refactor/Program.cs
+++ Devising Methods/Refactor/Program.cs
@@ -1,1 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Refactor";
+            computeFactorials(1, 8);
+            Console.ReadKey();
+
+        }
+
+        static int factorial(int num)
+        {
+            return (num == 1) ? 1 : (factorial(num - 1) * num);
+
+        }
+        static void computeFactorials( int num, int max)
+        {
+            while(num <= max)
+            {
+                Console.Write("Factorial Of " + num + " : ");
+                Console.WriteLine(factorial(num));
+                num++ ;
+            }
+        }
+    }
+}
